80d63abad7a517871645927a22fbe2e550a29a2a
Dewaldman <Dewaldman@outlook.com>
2019-05-27 19:00:51 -0300

    rm -r Just4fun

--- Just4Fun/GIT_conf.py
+++
@@ -1,9 +0,0 @@
-import os
-
-email = input('email: ')
-
-name = 'Dewaldman'
-
-os.system('git config --global user.email {}'.format(email))
-
-os.system('git config --global user.name {}'.format(name))
